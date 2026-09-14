048021e092e2bd253ee70c72ddbcf335a9061623
Viet Hoang <viethoang170504@gmail.com>
2026-09-14 14:09:46 +0700

    refactor: remove obsolete files

--- src/pages/products/ProductDetailPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ProductDetail } from "@/features/products";
-
-const ProductDetailPage: React.FC = () => {
-  return <ProductDetail />;
-};
-
-export default ProductDetailPage;
